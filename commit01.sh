d1a9e1f4460b1183b5c7be7b52ae724e14f863e7
你的GitHub用户名 <euoggu@163.com>
2024-12-30 10:28:37 +0800

    [action] 自动提交 - 2024-12-30 10:28:37

---
+++ commit01.sh
@@ -0,0 +1,24 @@
+# export https_proxy="http://172.20.10.1:1082"
+# export http_proxy="http://172.20.10.1:1082"
+export https_proxy=http://127.0.0.1:7897 http_proxy=http://127.0.0.1:7897 all_proxy=socks5://127.0.0.1:7897
+
+# Git 操作
+echo "开始 Git 操作..."
+
+# 添加所有更改的文件
+git add .
+
+# 获取当前时间作为提交信息和标签
+current_time=$(date "+%Y-%m-%d %H:%M:%S")
+version_tag="v$(date "+%Y%m%d%H%M%S")"  # 创建一个基于时间戳的版本标签
+
+# 添加触发 GitHub Action 的关键词
+commit_message="[action] 自动提交 - ${current_time}"
+
+# 提交更改
+git commit -m "$commit_message"
+
+# 推送到远程仓库
+git push origin main
+
+echo "Git 操作完成！"
